1b4d39cf001f7e897d79334ad97696fdf95d5dd2
Ryan Dunn <ryan.ddunn97@gmail.com>
2022-02-03 11:18:58 -0700

    fully added db

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,7 +5,7 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
-def get_deb():
+def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -20,3 +20,21 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('UTF-8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and crete new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
